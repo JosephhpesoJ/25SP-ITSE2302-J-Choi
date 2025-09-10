4a48839a27d96156a91faf9fe9620ba396d61e71
JosephhpesoJ <Jchoi100609@gmail.com>
2025-09-10 13:58:58 -0500

    Add files via upload

---
+++ Javascript/script.js
@@ -0,0 +1,36 @@
+// Store first and last name
+var firstName = "Joseph";
+var lastName = "Choi";
+
+// Store a sentence
+var sentence = "I am building a cool website for my project.";
+
+// Boolean variable
+var theTruth = true;
+
+// Number variables
+var num1 = 10;
+var num2 = 15;
+var num3 = 25;
+
+// Display first + last name w/space
+console.log("Full name: " + firstName + " " + lastName);
+
+// Display sentence
+console.log(
+  "The length of my sentence is: " + sentence.length + " characters long."
+);
+
+// Sum of num1 and num2
+console.log(
+  "The sum of " + num1 + " and " + num2 + " is equal to " + num3 + "."
+);
+
+// Boolean value + type
+console.log(
+  "The variable theTruth is storing the value " +
+    theTruth +
+    ". The data type of theTruth is " +
+    typeof theTruth +
+    "."
+);
